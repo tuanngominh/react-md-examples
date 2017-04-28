f42cb3ff854f2748f2cdb21941589ccf6df8c937
Tuan Ngo <tuan@trexanhlab.com>
2017-04-28 09:20:45 +0700

    remove vertical scrollbar when the content doesn't exceed the viewport yet

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import configureStore from './store/configureStore'
 import {Provider} from 'react-redux';
 import '../node_modules/react-md/dist/react-md.amber-blue.min.css'
 import Root from './Root';
+import './index.css';
 
 const store = configureStore();
 ReactDOM.render(
